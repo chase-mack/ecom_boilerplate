25ef8699a6dc847cf43572852b0d4be1cf018aac
Chase Mack <chase.mack@yahoo.com>
2023-07-12 08:23:54 -0400

    Started login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function LoginPage() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.heading}>Login to your account please.</h1>
+    </main>
+  );
+}

c353d065811ffa61155c7caa0b4cec8fd3a97a1b
Chase Mack <chase.mack@yahoo.com>
2023-07-13 09:25:03 -0400

    Added a login

---
+++ src/app/login/login.tsx
@@ -0,0 +1,79 @@
+"use client";
+import styles from "./page.module.css";
+import { useState, ChangeEvent, FormEvent } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const data = await getUserData(username, password);
+      const userToken = data.token;
+    } catch (error) {
+      setError("Invalid username or password");
+    }
+  };
+
+  return (
+    <div>
+      <h1 className={styles.heading}>Login</h1>
+      <div className={styles.formContainer}>
+        {error && <p>{error}</p>}
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="username"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <button className={styles.submit} type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export async function getUserData(username: string, password: string) {
+  const headers = {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = await fetch("https://fakestoreapi.com/auth/login", headers);
+  const data = await response.json();
+
+  return data;
+}
